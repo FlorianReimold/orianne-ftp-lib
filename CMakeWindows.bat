967dddc5a907c83addfa57e62d48cc8df822599e
Florian Reimold <11774314+FlorianReimold@users.noreply.github.com>
2019-07-23 13:45:03 +0200

    Added Development bat file for cmaking on windows

---
+++ CMakeWindows.bat
@@ -0,0 +1,8 @@
+mkdir build
+cd build
+set "BOOSTROOT=D:/Projects/Grid/grid_external/boost/1.62.0"
+set "BOOST_INCLUDEDIR=D:/Projects/Grid/grid_external/boost/1.62.0/include"
+set "BOOST_LIBRARYDIR=D:/Projects/Grid/grid_external/boost/1.62.0/lib/v140/Win32"
+
+cmake ..
+cd ..
